86c4cdf7d785b59487e4dff1567588c5bb67cb2b
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-06-13 17:40:48 +0700

    Install Tailwind css + Material UI, Add Navigation component

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './customer/components/nvagivation/Navigation';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation />
+      <div>
+        Home
+      </div>
+    </div>
+  );
+}
+
+export default App;
